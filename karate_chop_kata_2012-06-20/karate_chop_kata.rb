11f686593271ab1d58710af09ca1b1b5a5c9c9a7
Damon Butler <AMER\dbutler@gettyimages.com>
2012-06-19 16:01:50 -0500

    Added another chop method.

--- karate_chop_kata_2012-06-20/karate_chop_kata.rb
+++ karate_chop_kata_2012-06-20/karate_chop_kata.rb
@@ -20,6 +20,37 @@ def chop(int, array, idx=0)
   end
 end
 
-if __FILE__ == $0
-  puts chop(5, [1,3,5])
+#
+# looping
+#
+%Q(
+def chop(int, array)
+  if array.length == 0 || int < array[0] || int > array[-1]
+    return -1
+  end
+
+  idx = 0
+  while array.length > 0 && int >= array[0] && int <= array[-1]
+    # the integer hasn't yet been found, chop off the top half
+    midpoint = (array.length/2).to_i
+    top = array[0..(midpoint-1)]
+    if int > top[-1]
+      # the integer is in the bottom half of the array
+      array = array[midpoint..array.length]
+      idx += top.length
+    elsif int == top[-1]
+      # the integer has been found, and the array is 1 or 2 elements in length
+      if top.length > 1
+        idx += top.length - 1
+      end
+      return idx
+    else
+      # the integer is somewhere in the top half
+      array = top
+    end
+  end
+
+  # if loop wasn't entered, the desired integer wasn't present in array
+  return -1
 end
+)
