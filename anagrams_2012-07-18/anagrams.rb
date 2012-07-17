5cf3693b2063a8a12970fcf15828871754603228
Damon Butler <AMER\dbutler@gettyimages.com>
2012-07-17 18:39:22 -0500

    Added anagrams.rb.

---
+++ anagrams_2012-07-18/anagrams.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+f = File.new('/usr/share/dict/words')
+all_words = f.read()
+
+all_anagrams = []
+f.seek(0)
+f.readlines.each_with_index do |word, i|
+  anagrams = []
+  letters = word.strip.downcase.chars.to_a
+  if letters.length < 5
+    permutations = letters.permutation.to_a.uniq
+    puts "#{i} #{word.strip}: #{permutations.length} potential anagrams..."
+    permutations.each do |p|
+      w = p.join
+      re = Regexp.new("^#{w}$", Regexp::IGNORECASE + Regexp::MULTILINE)
+      anagrams << w if all_words.match(re)
+    end
+    all_anagrams << anagrams.sort if anagrams.length > 1
+  end
+  break if i > 1000
+end
+
+all_anagrams = all_anagrams.uniq.sort { |a,b| b.length <=> a.length }
+all_anagrams.each do |a|
+  puts a.inspect
+end
