8123e9d3f76f8f380032a4a13f4e571d9b1b79cd
Damon Butler <AMER\dbutler@gettyimages.com>
2012-08-05 09:58:01 -0500

    Added 100 doors quiz.

---
+++ 100_doors_2012-08-08.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+states = { 0 => 'closed', 1 => 'open' }
+
+doors = (1..100).map { |n| 0 } # all doors start closed
+(1..100).each do |pass|
+  doors.each_with_index do |door,i|
+    if (i + 1) % pass == 0
+      doors[i] = (doors[i] + 1) % 2
+    end
+  end
+end
+
+doors.each_with_index do |door,i|
+  puts "Door ##{i+1}: #{states[door]}"
+end
