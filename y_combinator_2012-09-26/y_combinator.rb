709222dbd8874ac40c2a4f57ec323fb0a83bd9fc
Damon Butler <AMER\dbutler@gettyimages.com>
2012-10-15 13:47:08 -0500

    Slight modifications during presentation.

--- y_combinator_2012-09-26/y_combinator.rb
+++ y_combinator_2012-09-26/y_combinator.rb
@@ -8,17 +8,6 @@ def factorial_recursive(n)
   end
 end
 
-def factorial_y(f)
-  lambda do |n|
-    if n == 0
-      return 1
-    else
-      return n * f.call(n-1)
-    end
-  end
-end
-
-
 def fibonacci_recursive(n)
   if n == 0
     return 0
@@ -26,6 +15,18 @@ def fibonacci_recursive(n)
     return 1
   else
     return fibonacci(n-1) + fibonacci(n-2)
+  end
+end
+
+#####
+
+def factorial_y(f)
+  lambda do |n|
+    if n == 0
+      return 1
+    else
+      return n * f.call(n-1)
+    end
   end
 end
 
@@ -41,16 +42,22 @@ def fibonacci_y(f)
   end
 end
 
+def y(f)
+  lambda do |x|
+    x.call(x)
+  end
+end
+
 #####
 
 def factorial(n)
   factorial_recursive(n)
-  #fatorial_y(n)
+  #y(factorial_y(n))
 end
 
 def fibonacci(n)
   fibonacci_recursive(n)
-  #fibonacci_y(n)
+  #y(fibonacci_y(n))
 end
 
 
